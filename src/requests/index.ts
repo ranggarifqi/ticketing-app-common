a8479862f4e6845caec75248e5be482a638b7275
Rangga Rifqi <rangga@ranggarifqi.com>
2022-05-20 07:40:28 +0800

    Updates

--- src/requests/index.ts
+++ src/requests/index.ts
@@ -1,5 +1,5 @@
 import { Request } from "express";
-import { UserJWTPayload } from "../middlewares";
+import { UserJWTPayload } from "../index";
 
 export interface RequestWithCredential extends Request {
   currentUser?: UserJWTPayload;
