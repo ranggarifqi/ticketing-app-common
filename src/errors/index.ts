3dec2178a475ae507d6ca318cfa15db53186adf1
Rangga Rifqi <rangga@ranggarifqi.com>
2022-06-14 06:49:43 +0800

    Updates

--- src/errors/index.ts
+++ src/errors/index.ts
@@ -4,3 +4,4 @@ export * from "./not-found-error";
 export * from "./request-validation-error";
 export * from "./unauthorized-error";
 export * from "./custom-error";
+export * from "./forbidden-error";
